55c404ecb357cdcce711ba773bcee5cab2440a1a
SalihBulmaz <bulmaz19@itu.edu.tr>
2022-07-26 17:05:02 +0300

    added validation for movie model

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -8,15 +8,27 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; } = string.Empty;
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
         public string Genre { get; set; } = string.Empty;
 
         [Column(TypeName = "decimal(18, 2)")]
+        [DataType(DataType.Currency)]
+        [Range(1, 100)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; } = string.Empty;
     }
 }
